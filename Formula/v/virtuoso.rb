3e196a46a541da4b72041d8cc5ee82a84b31e63c
Michael Cho <michael@michaelcho.dev>
2026-02-17 15:21:33 -0500

    virtuoso: use `zlib-ng-compat` on Linux

--- Formula/v/virtuoso.rb
+++ Formula/v/virtuoso.rb
@@ -32,11 +32,11 @@ class Virtuoso < Formula
   uses_from_macos "gperf" => :build
   uses_from_macos "python" => :build
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "net-tools" => :build
     depends_on "xz" # for liblzma
+    depends_on "zlib-ng-compat"
   end
 
   conflicts_with "unixodbc", because: "both install `isql` binaries"
